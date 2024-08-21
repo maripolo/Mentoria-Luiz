31a30632b91bb6abef8dfd351837096d0583e4c7
Marina Polo <142904464+maripolo@users.noreply.github.com>
2024-08-21 14:11:14 -0300

    Add files via upload

---
+++ exercicio1/assets/javascript/script.js
@@ -0,0 +1,18 @@
+//window.onload = function(){
+  //  alert("Olá mundo!");
+//};
+
+function menuShow(){
+    let menuMobile = document.querySelector('.mobile-menu');
+    if(menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+        document.querySelector('.icon').src="/assets/img/menu_white_36dp.svg";
+    } else{
+        menuMobile.classList.add('open');
+        document.querySelector('.icon').src="/assets/img/menu_white_36dp.svg";
+    }
+}
+
+function botaoVermelho(){
+    alert("você tem uma nova solicitação");
+}
