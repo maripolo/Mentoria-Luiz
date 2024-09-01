3b39a509f96546c587e711a2480ae031c6a1e111
Marina Polo <142904464+maripolo@users.noreply.github.com>
2024-09-01 20:50:37 -0300

    fix:
    Troca de código de HTML e CSS

--- exercicio1/assets/javascript/script.js
+++ exercicio1/assets/javascript/script.js
@@ -1,7 +1,8 @@
 //window.onload = function(){
   //  alert("Olá mundo!");
-//};
+  //} comentário em linha;
 
+/*
 function toggleMenu() {
     const menu = document.querySelector('.mobile-menu');
     menu.classList.toggle('open');
@@ -9,4 +10,19 @@ function toggleMenu() {
 
 function botaoVermelho(){
     alert("você tem uma nova solicitação");
+    Comentário em bloco
+}*/
+
+/**
+ * 
+ * comentários em código
+ */
+
+function toggleMenu() {
+  const menu = document.querySelector('.mobile-menu');
+  menu.classList.toggle('open');
+}
+
+function botaoVermelho(){
+  alert("você tem uma nova solicitação");
 }
